5e93e13d586a5d9c94bed623a5cfc516eb7183a2
cominan <cominan.cominan@gmail.com>
2023-01-16 11:19:02 +0700

    update server

--- server.js
+++ server.js
@@ -41,7 +41,26 @@ const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT || 3000
 
 server.use(middlewares)
+
+
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+})
+
+// Use default router
 server.use(router)
-server.listen(PORT,() => {
+
+server.listen(PORT, () => {
     console.log('json start on PORT', PORT);
 })
